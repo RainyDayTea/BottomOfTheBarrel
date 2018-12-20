53fb9a5342e1f90368bb2c2795dbc25d78c7592a
Alfglass <rainyday.tea.party@gmail.com>
2018-12-20 00:33:19 -0500

    Added vector classes

---
+++ src/Matrix2D.java
@@ -0,0 +1,21 @@
+public class Matrix2D {
+	
+	double m00, m01, m10, m11;
+	
+	Matrix2D() {
+		m00 = 0;
+		m01 = 1;
+		m10 = 1;
+		m11 = 0;
+	}
+	
+	Matrix2D(double m00, double m01, double m10, double m11) {
+		this.m00 = m00;
+		this.m01 = m01;
+		this.m10 = m10;
+		this.m11 = m11;
+	}
+	
+	
+	
+}
---
+++ src/Vector2D.java
@@ -0,0 +1,45 @@
+import java.util.Vector;
+
+public class Vector2D {
+	
+	double x;
+	double y;
+	
+	Vector2D() {
+		x = 0;
+		y = 0;
+	}
+	Vector2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void add(Vector2D other) {
+		this.x += other.x;
+		this.y += other.y;
+	}
+	
+	public void sub(Vector2D other) {
+		this.x -= other.x;
+		this.y -= other.y;
+	}
+	
+	public void scale(double scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+	}
+
+	public void set(Vector2D vec) {
+		this.x = vec.x;
+		this.y = vec.y;
+	}
+	
+	public double dotProduct(Vector2D other) {
+		return this.x * other.x + this.y * other.y;
+	}
+	
+	public double getMagnitude() {
+		return Math.sqrt(x * x + y * y);
+	}
+	
+}
