6a1c2d4489f52e5bef950efb9fa6d2e5b7a3233a
RainyDayTea <rainyday.tea.party@gmail.com>
2019-01-07 14:47:06 -0500

    More geometry added, updated UML

---
+++ src/Circle.java
@@ -0,0 +1,30 @@
+
+public class Circle {
+
+	Vector2D pos;
+	double radius;
+
+	public Circle() {
+		this.pos = new Vector2D();
+		this.radius = 1;
+	}
+
+	public Circle(Vector2D pos) {
+		this.pos = pos;
+		radius = 1;
+	}
+
+	public Circle(Vector2D pos, double radius) {
+		this.pos = pos;
+		this.radius = radius;
+	}
+
+	public double getArea() {
+		return radius * radius * Math.PI;
+	}
+
+	public double getCir() {
+		return 2 * Math.PI * radius;
+	}
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,24 @@
+public class Rectangle {
+
+	Vector2D pos;
+	Vector2D size;
+
+	public Rectangle() {
+		this.pos = new Vector2D();
+		this.size = new Vector2D(1, 1);
+	}
+
+	public Rectangle(Vector2D pos, Vector2D size) {
+		this.pos = pos;
+		this.size = size;
+	}
+
+	public double getArea() {
+		return this.size.x * this.size.y;
+	}
+
+	public double getPerimeter() {
+		return this.size.x * 2 + this.size.y * 2;
+	}
+
+}
