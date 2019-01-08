4931e64c1d075447f421879f67cc23667da19df1
RainyDayTea <rainyday.tea.party@gmail.com>
2019-01-08 13:49:41 -0500

    Started QuadTree implementation, to be finalized later

---
+++ src/QuadTree.java
@@ -0,0 +1,12 @@
+/**
+ * Container class for a quadtree. Used for hitboxes and collision detection.
+ *
+ * WIP - Finished once QuadTreeNode is finished.
+ *
+ * @author Jake Zhao
+ */
+public class QuadTree {
+
+	private QuadTreeNode root;
+
+}
---
+++ src/QuadTreeNode.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+/**
+ * Represents a node of the quadtree.
+ *
+ * WIP - Finished once hitboxes are finalized.
+ *
+ * @author Jake Zhao
+ */
+public class QuadTreeNode {
+
+	private QuadTreeNode q1;
+	private QuadTreeNode q2;
+	private QuadTreeNode q3;
+	private QuadTreeNode q4;
+	private Vector2D size;
+	private ArrayList<Vector2D> objects;
+
+	public static final int SPLIT_THRESHOLD = 5;
+
+	public QuadTreeNode getQ1() {
+		return q1;
+	}
+
+	public QuadTreeNode getQ2() {
+		return q2;
+	}
+
+	public QuadTreeNode getQ3() {
+		return q3;
+	}
+
+	public QuadTreeNode getQ4() {
+		return q4;
+	}
+
+	public ArrayList<Vector2D> getObjects() {
+		return objects;
+	}
+
+	public void split() {}
+
+	public void merge() {}
+
+	public void add() {}
+
+	public void remove() {}
+
+	public void clear() {}
+
+	public boolean isLeafNode() {
+		if (q1 == null && q2 == null && q3 == null && q4 == null) return true;
+		return false;
+	}
+
+}
