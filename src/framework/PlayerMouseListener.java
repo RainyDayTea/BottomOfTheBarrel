1db600fb9b3a10ef3e22175b9df10bff2b1aa3de
RainyDayTea <rainyday.tea.party@gmail.com>
2019-01-11 14:36:26 -0500

    Generic update

--- src/framework/PlayerMouseListener.java
+++ src/framework/PlayerMouseListener.java
@@ -4,32 +4,46 @@ import framework.geom.Vector2D;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-public class PlayerMouseListener implements MouseListener {
+public class PlayerMouseListener implements MouseListener, MouseMotionListener {
 
 	private boolean lmbDown;
 	private boolean rmbDown;
 	private Vector2D position;
 
-	public void mouseClicked(MouseEvent e) {
-
+	public PlayerMouseListener() {
+		lmbDown = false;
+		rmbDown = false;
+		position = new Vector2D();
 	}
 
+	@Override
+	public void mouseClicked(MouseEvent e) { }
+
+	@Override
 	public void mousePressed(MouseEvent e) {
 		if (e.getButton() == 1) lmbDown = true;
 		if (e.getButton() == 2) rmbDown = true;
 	}
 
+	@Override
 	public void mouseReleased(MouseEvent e) {
 		if (e.getButton() == 1) lmbDown = false;
 		if (e.getButton() == 2) rmbDown = false;
 	}
 
-	public void mouseEntered(MouseEvent e) {
+	@Override
+	public void mouseEntered(MouseEvent e) {}
 
-	}
+	@Override
+	public void mouseExited(MouseEvent e) {}
 
-	public void mouseExited(MouseEvent e) {
+	@Override
+	public void mouseDragged(MouseEvent e) {}
 
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		this.position = new Vector2D(e.getXOnScreen(), e.getYOnScreen());
 	}
 }
